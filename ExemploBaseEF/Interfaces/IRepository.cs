ffe45719d103746f34f01e14e031e812d8b47156
valentinovalentereis <54193367+valentinovalentereis@users.noreply.github.com>
2019-08-28 21:53:00 -0300

    Add files via upload

---
+++ ExemploBaseEF/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace ExemploBaseEF.Interfaces
+{
+    using ExemploBaseEF.Entities;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public interface IRepository<T> where T : BaseEntity
+    {
+
+        void Insert(T obj);
+
+        void Update(T obj);
+
+        void Delete(long id);
+
+        T SelectById(long id);
+
+        IList<T> SelectAll();
+
+        IQueryable<T> GetQuery();
+    }
+}
