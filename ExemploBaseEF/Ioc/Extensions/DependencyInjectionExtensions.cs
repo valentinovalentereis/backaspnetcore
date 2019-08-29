cfcdc405ffcebd405efc059597a1f8c61fc911ca
valentinovalentereis <54193367+valentinovalentereis@users.noreply.github.com>
2019-08-28 21:53:56 -0300

    Add files via upload

---
+++ ExemploBaseEF/Ioc/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace ExemploBaseEF.IoC.Extensions
+{
+    using ExemploBaseEF.IoC;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DependencyInjectionExtensions
+    {
+
+        /// <summary>
+        /// Injeta as dependências do projeto
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddDependencyInjection(this IServiceCollection services)
+        {
+            NativeInjectorBootStrapper.RegisterServices(services);
+            ExemploBaseEFInjectorBootStrapper.RegisterServices(services);
+        }
+    }
+}
