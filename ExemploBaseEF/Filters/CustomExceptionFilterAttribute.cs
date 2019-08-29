d4ec2dc97dbfee714bdd0541ac81d119d02c01dc
valentinovalentereis <54193367+valentinovalentereis@users.noreply.github.com>
2019-08-28 21:39:44 -0300

    Add files via upload

---
+++ ExemploBaseEF/Filters/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,36 @@
+﻿using ExemploBaseEF.APIException;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace ExemploBaseEF.Filters
+{
+    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            if (exception is UnauthorizedAccessException)
+                SetExceptionResult(context, exception, HttpStatusCode.Unauthorized);
+            else if (exception is Exception)
+                SetExceptionResult(context, exception, HttpStatusCode.BadRequest);
+            else
+                SetExceptionResult(context, exception, HttpStatusCode.InternalServerError);
+        }
+
+        private static void SetExceptionResult(ExceptionContext context, Exception exception, HttpStatusCode code)
+        {
+            context.Result = new JsonResult(new ApiException(exception))
+            {
+                StatusCode = (int)code
+            };
+        }
+    }
+}
